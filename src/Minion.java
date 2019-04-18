25251b191571d51746bd04e73466558b5c42efb6
alibalapour <42287060+alibalapour@users.noreply.github.com>
2019-04-19 00:20:48 +0430

    AliBalapour : some minions added 2

--- src/Minion.java
+++ src/Minion.java
@@ -29,6 +29,14 @@ public class Minion extends NonSpellCards
         Minion minion13 = new Minion("Dive Sepid" ,300 , 9 ,14 , 10 , ImpactType.hybrid , 7 , null , null);
         Minion minion14 = new Minion("Ghoul Sang Andaz" ,300 , 9 ,12 , 12 , ImpactType.ranged , 7 , null , null);
         Minion minion15 = new Minion("Oghab" ,200 , 2 ,0 , 2 , ImpactType.ranged , 3 , /*todo*/ , TimeToActivateSpecialPower.passive);
+        Minion minion16 = new Minion("Div GorazSavar" ,300 , 6 ,16 , 8 , ImpactType.melee , 0 , null , null);
+        Minion minion17 = new Minion("Ghoul TakCheshm" ,500 , 7 ,12 , 11 , ImpactType.hybrid , 0, null/*todo*/ , TimeToActivateSpecialPower.onDeath);
+        Minion minion18 = new Minion("Mar Sammi" ,300 , 4 ,5 , 6 , ImpactType.ranged , 4 ,/*todo*/ ,  TimeToActivateSpecialPower.onAttack);
+        Minion minion19 = new Minion("Ezhdehaye Atash Andaz" ,250 , 5 ,9 , 5 , ImpactType.ranged , 4 ,null ,  null);
+        Minion minion20 = new Minion("Shir Darande" ,600 , 2 ,1 , 8 , ImpactType.melee , 0 ,/*todo*/ ,  TimeToActivateSpecialPower.onAttack);
+
+
+
 
 
     }
