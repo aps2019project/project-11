88ab6028a86a1cfd4228da0e53702cf8ec85d891
alibalapour <42287060+alibalapour@users.noreply.github.com>
2019-04-18 22:52:51 +0430

    AliBalapour : some minions added 1

--- src/Minion.java
+++ src/Minion.java
@@ -4,6 +4,19 @@ public class Minion extends NonSpellCards
 {
     private static ArrayList<Minion> minions = new ArrayList<>();
 
+    Minion(String name , int price , int MP , int HP , int AP , ImpactType impactType , int rangeOfAttack , Spell specialPower , TimeToActivateSpecialPower timeToActivateSpecialPower ){
+        this.setCardName(name);
+        this.setPrice(price);
+        this.setRequiredMP(MP);
+        this.setDefaultHP(HP);
+        this.setDefaultAP(AP);
+        this.setImpactType(impactType);
+        this.setMaxAttackRange(rangeOfAttack);
+        this.setSpecialPower(specialPower);
+        //TimeToActivateSpecialPower
+        minions.add(this);
+    }
+
     public static ArrayList<Minion> getMinions()
     {
         return minions;
@@ -11,6 +24,12 @@ public class Minion extends NonSpellCards
 
     public static void setMinions()
     {
+        Minion minion11 = new Minion("Gorzdar Torani" ,450 , 2 ,3 , 10 , ImpactType.melee , 0 , null , null);
+        Minion minion12 = new Minion("Shahzade Torani" ,800 , 6 ,6 , 10 , ImpactType.melee , 0 , null /*todo*/ , TimeToActivateSpecialPower.combo);
+        Minion minion13 = new Minion("Dive Sepid" ,300 , 9 ,14 , 10 , ImpactType.hybrid , 7 , null , null);
+        Minion minion14 = new Minion("Ghoul Sang Andaz" ,300 , 9 ,12 , 12 , ImpactType.ranged , 7 , null , null);
+        Minion minion15 = new Minion("Oghab" ,200 , 2 ,0 , 2 , ImpactType.ranged , 3 , /*todo*/ , TimeToActivateSpecialPower.passive);
+
 
     }
 
