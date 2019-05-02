0de91e63f9d544faa6ff715d1e43b7ccc001ec92
F-Hp <smahdifaghih2001@gmail.com>
2019-05-02 17:21:53 +0430

    convert ItemActivateTime field to ArrayList

--- src/Model/ItemEffect.java
+++ src/Model/ItemEffect.java
@@ -6,29 +6,35 @@ public class ItemEffect
 {
     private ArrayList<ItemChange> itemChanges = new ArrayList<>();
     private ArrayList<ItemTarget> itemTargets = new ArrayList<>();
-    private ItemActivateTime itemActivateTimes;
+    private ArrayList<ItemActivateTime> itemActivateTimes = new ArrayList<>();
 
-    public ArrayList<ItemChange> getItemChanges() {
+    public ArrayList<ItemChange> getItemChanges()
+    {
         return itemChanges;
     }
 
-    public void addItemChange(ItemChange itemChange) {
+    public void addItemChange(ItemChange itemChange)
+    {
         itemChanges.add(itemChange);
     }
 
-    public ArrayList<ItemTarget> getItemTargets() {
+    public ArrayList<ItemTarget> getItemTargets()
+    {
         return itemTargets;
     }
 
-    public void addItemTarget(ItemTarget itemTarget) {
+    public void addItemTarget(ItemTarget itemTarget)
+    {
         itemTargets.add(itemTarget);
     }
 
-    public ItemActivateTime getItemActivateTimes() {
+    public ArrayList<ItemActivateTime> getItemActivateTimes()
+    {
         return itemActivateTimes;
     }
 
-    public void setItemActivateTimes(ItemActivateTime itemActivateTimes) {
-        this.itemActivateTimes = itemActivateTimes;
+    public void addItemActivateTime(ItemActivateTime itemActivateTime)
+    {
+        itemActivateTimes.add(itemActivateTime);
     }
 }
