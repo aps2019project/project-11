b93761ef6aa46c2a20063fe9ffb862b669795b97
alibalapour <alibalapour93.ab@gmail.com>
2019-06-07 00:47:09 -0700

    end turn button changed

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -1,7 +1,11 @@
 package View;
 
+import Model.Battle;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
+    public void endTurn(){
+        Battle.getCurrentBattle().endTurn();
+    }
 
 }
