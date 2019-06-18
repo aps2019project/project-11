10af5053690b008e0320c3de6ce5cf2dad9273d1
alibalapour <alibalapour93.ab@gmail.com>
2019-06-19 01:30:36 +0430

    selecting Card added

--- src/Controller/BattleFieldController.java
+++ src/Controller/BattleFieldController.java
@@ -4,6 +4,7 @@ import Model.Battle;
 import Model.Card;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class BattleFieldController extends Thread {
@@ -20,22 +21,43 @@ public class BattleFieldController extends Thread {
         Pane[] firstPlayerHandPanes = Battle.getCurrentBattle().getFirstPlayerHandPanes();
         Pane[] secondPlayerHandPanes = Battle.getCurrentBattle().getSecondPlayerHandPanes();
 
-        for(Pane pane : firstPlayerHandPanes){
-            pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        for(int number =0 ; number < firstPlayerHandPanes.length ; number++){
+            int finalNumber = number;
+            firstPlayerHandPanes[number].setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
+                    if(isCardSelected){
+                        //todo to Show error
+                    }
+                    else {
+                        setCardSelected(true);
+                        setSelectedCard(Battle.getCurrentBattle().getFirstPlayer().getHand().getCards().get(finalNumber));
+                    }
                 }
             });
         }
 
-        for(Pane pane : secondPlayerHandPanes){
-            pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        for(int number =0 ; number < secondPlayerHandPanes.length ; number++){
+            int finalNumber = number;
+            secondPlayerHandPanes[number].setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
+                    if(isCardSelected){
+                        //todo to show error
+                    }
+                    else
+                    {
+                        setCardSelected(true);
+                        setSelectedCard(Battle.getCurrentBattle().getSecondPlayer().getHand().getCards().get(finalNumber));
+                    }
                 }
             });
         }
+
+        GridPane gridPane = Battle.getCurrentBattle().getBattleFieldGridPane();
     }
+
+
 
     public Card getSelectedCard() {
         return selectedCard;
