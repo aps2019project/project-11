313732cba8fd235f099ef0aac6b3a4dfc6e95793
alibalapour <alibalapour93.ab@gmail.com>
2019-07-01 22:13:17 +0430

    ReceiveMessageServer and client and sendMassage server and client created but are same

---
+++ src/Network/ReceiveMessageClient.java
@@ -0,0 +1,50 @@
+package Network;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ReceiveMessageClient extends Thread
+{
+    private ArrayList<CommandReceivedListener> listeners = new ArrayList<>();
+    private InputStream inputStream;
+
+    ReceiveMessageClient(InputStream inputStream)
+    {
+        this.inputStream = inputStream;
+    }
+
+    @Override
+    public void run()
+    {
+        Scanner scanner = new Scanner(inputStream);
+        while (true)
+        {
+            try
+            {
+                String inputCommand = scanner.nextLine();
+                for (CommandReceivedListener listener : listeners)
+                {
+                    try
+                    {
+                        listener.onMessageReceived(inputCommand);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            catch (NoSuchElementException ignored)
+            {
+
+            }
+        }
+    }
+
+    public void addListener(CommandReceivedListener listener)
+    {
+        listeners.add(listener);
+    }
+}
