976ddc14b6b786b5d319bc2c4978549f449edb22
F-Hp <smahdifaghih2001@gmail.com>
2019-07-09 16:50:25 +0430

    fix some bugs in serverGraphic

--- src/Network/ServerGraphic.java
+++ src/Network/ServerGraphic.java
@@ -20,7 +20,8 @@ import javafx.stage.Stage;
 
 import static javafx.scene.paint.Color.*;
 
-public class ServerGraphic extends Application implements Runnable{
+public class ServerGraphic extends Application implements Runnable
+{
     private Stage primaryStage;
     private Group rootServerMenu;
     private Scene sceneServerMenu;
@@ -33,75 +34,82 @@ public class ServerGraphic extends Application implements Runnable{
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception
+    {
         setPrimaryStage(primaryStage);
         makeMenu();
     }
 
-    private void makeMenu() {
+    private void makeMenu()
+    {
         Group rootServerMenu = new Group();
         setRootServerMenu(rootServerMenu);
         setBackGroundImage(rootServerMenu);
         Button showOnlinePlayers = new Button("Show Online Players");
-        showOnlinePlayers.relocate(220 , 250);
-        showOnlinePlayers.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        showOnlinePlayers.relocate(220, 250);
+        showOnlinePlayers.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
             @Override
-            public void handle(MouseEvent event) {
+            public void handle(MouseEvent event)
+            {
                 showOnlinePlayers();
             }
         });
 
         Button showCard = new Button("Show Cards");
-        showCard.relocate(220 , 280);
-        showCard.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        showCard.relocate(220, 280);
+        showCard.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
             @Override
-            public void handle(MouseEvent event) {
+            public void handle(MouseEvent event)
+            {
                 showCards();
             }
         });
-        rootServerMenu.getChildren().addAll(showCard , showOnlinePlayers);
-        Scene sceneServerMenu = new Scene(rootServerMenu , 500 , 500);
+        rootServerMenu.getChildren().addAll(showCard, showOnlinePlayers);
+        Scene sceneServerMenu = new Scene(rootServerMenu, 500, 500);
         setSceneServerMenu(sceneServerMenu);
         primaryStage.setScene(sceneServerMenu);
         primaryStage.show();
     }
 
-    private void showCards() {
+    private void showCards()
+    {
         Group rootCardsInServer = new Group();
         setBackGroundImage(rootCardsInServer);
-        ScrollPane scrollPaneShop = new ScrollPane();
+        ScrollPane scrollPane = new ScrollPane();
 
-        scrollPaneShop.setContent(rootCardsInServer);
-        scrollPaneShop.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPaneShop.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setContent(rootCardsInServer);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 
         int xPosition = 0, yPosition = 0, x = 0, y = 0;
-        setShopText(rootCardsInServer, "Heroes", 50);
-        for (Hero hero :Server.getHeroes())
+        setText(rootCardsInServer, "Heroes", 50);
+        for (Hero hero : Server.getHeroes())
         {
-            x = ROW_BLANK + (xPosition % 4) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
-            y = COLUMN_BLANK + yPosition / 4 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
+            x = ROW_BLANK + (xPosition % 2) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
+            y = COLUMN_BLANK + yPosition / 2 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
             xPosition++;
             yPosition++;
-            showNonSpellCards(rootCardsInServer, x, y, hero);
+            showCardAndItemImageAndFeatures(rootCardsInServer, x, y, hero.getCardName());
         }
 
         if (xPosition == 0)
         {
             y += BLANK_BETWEEN_CARDS;
-            yPosition += 4;
+            yPosition += 2;
         }
         xPosition = 0;
-        if (yPosition % 4 != 0)
+        if (yPosition % 2 != 0)
         {
-            yPosition = yPosition + 4 - yPosition % 4;
+            yPosition = yPosition + 2 - yPosition % 2;
         }
-        setShopText(rootCardsInServer,"Minions", y + CARDS_RECTANGLE_HEIGHT + 50);
+        setText(rootCardsInServer, "Minions", y + CARDS_RECTANGLE_HEIGHT + 50);
         for (Minion minion : Server.getMinions())
         {
-            x = ROW_BLANK + (xPosition % 4) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
-            y = 2 * COLUMN_BLANK - BLANK_BETWEEN_CARDS + yPosition / 4 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
-            showNonSpellCards(rootCardsInServer, x, y, minion);
+            x = ROW_BLANK + (xPosition % 2) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
+            y = 2 * COLUMN_BLANK - BLANK_BETWEEN_CARDS + yPosition / 2 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
+            showCardAndItemImageAndFeatures(rootCardsInServer, x, y, minion.getCardName());
             xPosition++;
             yPosition++;
         }
@@ -109,19 +117,20 @@ public class ServerGraphic extends Application implements Runnable{
         if (xPosition == 0)
         {
             y += CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS;
-            yPosition += 4;
+            yPosition += 2;
         }
         xPosition = 0;
-        if (yPosition % 4 != 0)
+        if (yPosition % 2 != 0)
         {
-            yPosition = yPosition + 4 - yPosition % 4;
+            yPosition = yPosition + 2 - yPosition % 2;
         }
-        setShopText(rootCardsInServer,"Spells", y + CARDS_RECTANGLE_HEIGHT + 50);
+        setText(rootCardsInServer, "Spells", y + CARDS_RECTANGLE_HEIGHT + 50);
         for (Spell spell : Server.getSpells())
         {
 
-            x = ROW_BLANK + (xPosition % 4) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
-            y = 3 * COLUMN_BLANK - 2 * BLANK_BETWEEN_CARDS + yPosition / 4 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
+            x = ROW_BLANK + (xPosition % 2) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
+            y = 3 * COLUMN_BLANK - 2 * BLANK_BETWEEN_CARDS + yPosition / 2 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
+            showCardAndItemImageAndFeatures(rootCardsInServer, x, y, spell.getCardName());
             xPosition++;
             yPosition++;
         }
@@ -129,57 +138,38 @@ public class ServerGraphic extends Application implements Runnable{
         if (xPosition == 0)
         {
             y += CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS;
-            yPosition += 4;
+            yPosition += 2;
         }
         else
         {
             xPosition = 0;
-            if (yPosition % 4 != 0)
+            if (yPosition % 2 != 0)
             {
-                yPosition = yPosition + 4 - yPosition % 4;
+                yPosition = yPosition + 2 - yPosition % 2;
             }
         }
-        setShopText(rootCardsInServer,"Items", y + CARDS_RECTANGLE_HEIGHT + 50);
+        setText(rootCardsInServer, "Items", y + CARDS_RECTANGLE_HEIGHT + 50);
         for (Item item : Server.getItems())
         {
-            x = ROW_BLANK + (xPosition % 4) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
-            y = 4 * COLUMN_BLANK - 3 * BLANK_BETWEEN_CARDS + yPosition / 4 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
+            x = ROW_BLANK + (xPosition % 2) * (CARDS_RECTANGLE_WIDTH + BLANK_BETWEEN_CARDS);
+            y = 4 * COLUMN_BLANK - 3 * BLANK_BETWEEN_CARDS + yPosition / 2 * (CARDS_RECTANGLE_HEIGHT + BLANK_BETWEEN_CARDS);
+            showCardAndItemImageAndFeatures(rootCardsInServer, x, y, item.getItemName());
             xPosition++;
             yPosition++;
         }
 
         backButton(rootCardsInServer, 20, 15);
 
-        Scene sceneShop = new Scene(rootCardsInServer , 500 , 500);
+        Scene sceneShop = new Scene(scrollPane, 500, 500);
         primaryStage.setScene(sceneShop);
         primaryStage.show();
     }
 
-    private void showNonSpellCards(Group root, int x, int y, NonSpellCard nonSpellCard)
+    private void showCardAndItemImageAndFeatures(Group root, int x, int y, String name)
     {
-        int AP = nonSpellCard.getDefaultAP();
-        int HP = nonSpellCard.getDefaultHP();
+        Item item = Item.findItem(name);
+        Card card = Card.findCard(name);
 
-        showCardAndItemImageAndFeatures(root, x, y, nonSpellCard.getCardName());
-
-        Text textAP = new Text(Integer.toString(AP));
-        textAP.setFont(Font.font(15));
-        textAP.setFill(RED);
-        textAP.setLayoutX(x + (CARDS_RECTANGLE_WIDTH - textAP.getLayoutBounds().getWidth()) / 2 - 40);
-        textAP.setLayoutY(y + 200);
-
-        Text textHP = new Text(Integer.toString(HP));
-        textHP.setFill(YELLOW);
-        textHP.setFont(Font.font(15));
-        textHP.setLayoutX(x + (CARDS_RECTANGLE_WIDTH - textHP.getLayoutBounds().getWidth()) / 2 + 40);
-        textHP.setLayoutY(y + 200);
-
-        root.getChildren().addAll(textAP, textHP);
-
-    }
-
-    private void showCardAndItemImageAndFeatures(Group root, int x, int y, String nameOrID)
-    {
         Image image = new Image("file:Cards.jpg");
         ImageView imageView = new ImageView(image);
 
@@ -190,16 +180,30 @@ public class ServerGraphic extends Application implements Runnable{
         stackPane.setAlignment(Pos.TOP_CENTER);
         stackPane.relocate(x, y);
 
-        Text textCardName = new Text(nameOrID);
+        Text textCardName = new Text(name);
         textCardName.setFont(Font.font(15));
         textCardName.setLayoutX(x + (rectangle.getWidth() - textCardName.getLayoutBounds().getWidth()) / 2);
         textCardName.setLayoutY(y + 160);
 
-        root.getChildren().addAll(stackPane, textCardName);
+        Text textSellCapacity = new Text();
+        if (card != null)
+        {
+            textSellCapacity.setText(Integer.toString(card.getCapacityOfSell()));
+        }
+        else if (item != null)
+        {
+            textSellCapacity.setText(Integer.toString(item.getCapacityOfSell()));
+        }
+        textSellCapacity.setFont(Font.font(15));
+        textSellCapacity.setFill(RED);
+        textSellCapacity.setLayoutX(x + (rectangle.getWidth() - textSellCapacity.getLayoutBounds().getWidth()) / 2);
+        textSellCapacity.setLayoutY(y + 200);
+
+        root.getChildren().addAll(stackPane, textCardName, textSellCapacity);
 
     }
 
-    private void setShopText(Group root, String str, int y)
+    private void setText(Group root, String str, int y)
     {
         Text text = new Text(str);
         text.setLayoutX((500 - text.getLayoutBounds().getWidth()) / 2 - 40);
@@ -208,36 +212,41 @@ public class ServerGraphic extends Application implements Runnable{
         root.getChildren().addAll(text);
     }
 
-    private void showOnlinePlayers() {
+    private void showOnlinePlayers()
+    {
         Group rootShowOnlinePlayers = new Group();
         setBackGroundImage(rootShowOnlinePlayers);
-        int counter = 0 ;
+        int counter = 0;
         Text onlinePlayerText = new Text("Online Players");
-        onlinePlayerText.setFont(Font.font("verdana" , 25));
-        onlinePlayerText.relocate(170 , 5);
+        onlinePlayerText.setFont(Font.font("verdana", 25));
+        onlinePlayerText.relocate(170, 5);
         rootShowOnlinePlayers.getChildren().add(onlinePlayerText);
-        for(Account account :Server.getAccounts()){
-            if(account.getAuthToken() != null){
+        for (Account account : Server.getAccounts())
+        {
+            if (account.getAuthToken() != null)
+            {
                 Text text = new Text(account.getAccountName());
-                text.relocate(5 , 50 + counter * 10);
+                text.relocate(5, 50 + counter * 10);
                 rootShowOnlinePlayers.getChildren().add(text);
                 counter++;
             }
         }
 
-        backButton(rootShowOnlinePlayers , 400 , 400);
+        backButton(rootShowOnlinePlayers, 400, 400);
 
-        Scene sceneShowOnlinePlayers = new Scene(rootShowOnlinePlayers , 500 , 500);
+        Scene sceneShowOnlinePlayers = new Scene(rootShowOnlinePlayers, 500, 500);
         primaryStage.setScene(sceneShowOnlinePlayers);
         primaryStage.show();
     }
 
 
-    public Stage getPrimaryStage() {
+    public Stage getPrimaryStage()
+    {
         return primaryStage;
     }
 
-    private void setPrimaryStage(Stage primaryMessage) {
+    private void setPrimaryStage(Stage primaryMessage)
+    {
         this.primaryStage = primaryMessage;
     }
 
@@ -267,24 +276,29 @@ public class ServerGraphic extends Application implements Runnable{
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
         launch();
     }
 
 
-    public Group getRootServerMenu() {
+    public Group getRootServerMenu()
+    {
         return rootServerMenu;
     }
 
-    public void setRootServerMenu(Group rootServerMenu) {
+    public void setRootServerMenu(Group rootServerMenu)
+    {
         this.rootServerMenu = rootServerMenu;
     }
 
-    public Scene getSceneServerMenu() {
+    public Scene getSceneServerMenu()
+    {
         return sceneServerMenu;
     }
 
-    public void setSceneServerMenu(Scene sceneServerMenu) {
+    public void setSceneServerMenu(Scene sceneServerMenu)
+    {
         this.sceneServerMenu = sceneServerMenu;
     }
 }
