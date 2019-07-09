47ef305a9a2b0cf6090f987bab13fdb33eb50a86
alibalapour <alibalapour93.ab@gmail.com>
2019-07-09 10:40:40 +0430

    back button and background added to server graphic

--- src/Network/ServerGraphic.java
+++ src/Network/ServerGraphic.java
@@ -1,11 +1,14 @@
 package Network;
 
 import Model.Account;
+import Model.CommandType;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -13,6 +16,8 @@ import javafx.stage.Stage;
 
 public class ServerGraphic extends Application implements Runnable{
     private Stage primaryMessage;
+    private Group rootServerMenu;
+    private Scene sceneServerMenu;
 
 
     @Override
@@ -23,6 +28,8 @@ public class ServerGraphic extends Application implements Runnable{
 
     private void makeMenu() {
         Group rootServerMenu = new Group();
+        setRootServerMenu(rootServerMenu);
+        setBackGroundImage(rootServerMenu);
         Button showOnlinePlayers = new Button("Show Online Players");
         showOnlinePlayers.relocate(220 , 250);
         showOnlinePlayers.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -42,6 +49,7 @@ public class ServerGraphic extends Application implements Runnable{
         });
         rootServerMenu.getChildren().addAll(showCard , showOnlinePlayers);
         Scene sceneServerMenu = new Scene(rootServerMenu , 500 , 500);
+        setSceneServerMenu(sceneServerMenu);
         primaryMessage.setScene(sceneServerMenu);
         primaryMessage.show();
     }
@@ -52,6 +60,7 @@ public class ServerGraphic extends Application implements Runnable{
 
     private void showOnlinePlayers() {
         Group rootShowOnlinePlayers = new Group();
+        setBackGroundImage(rootShowOnlinePlayers);
         int counter = 0 ;
         Text onlinePlayerText = new Text("Online Players");
         onlinePlayerText.setFont(Font.font("verdana" , 25));
@@ -65,6 +74,9 @@ public class ServerGraphic extends Application implements Runnable{
                 counter++;
             }
         }
+
+        backButton(rootShowOnlinePlayers , 400 , 400);
+
         Scene sceneShowOnlinePlayers = new Scene(rootShowOnlinePlayers , 500 , 500);
         primaryMessage.setScene(sceneShowOnlinePlayers);
         primaryMessage.show();
@@ -79,8 +91,50 @@ public class ServerGraphic extends Application implements Runnable{
         this.primaryMessage = primaryMessage;
     }
 
+    private void setBackGroundImage(Group root)
+    {
+        Image backGroundImage = new Image("file:BackGround Images/simple-background-blue-simple-minimalism-wallpaper.jpg");
+        ImageView backGroundImageView = new ImageView(backGroundImage);
+        root.getChildren().add(backGroundImageView);
+    }
+
+    private Button backButton(Group root, int x, int y)
+    {
+        Button backButton = new Button("Back");
+        backButton.setFont(Font.font(25));
+        backButton.relocate(x, y);
+        backButton.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                primaryMessage.setScene(getSceneServerMenu());
+                makeMenu();
+            }
+        });
+        root.getChildren().add(backButton);
+        return backButton;
+    }
+
     @Override
     public void run() {
         launch();
+    }
+
+
+    public Group getRootServerMenu() {
+        return rootServerMenu;
+    }
+
+    public void setRootServerMenu(Group rootServerMenu) {
+        this.rootServerMenu = rootServerMenu;
+    }
+
+    public Scene getSceneServerMenu() {
+        return sceneServerMenu;
+    }
+
+    public void setSceneServerMenu(Scene sceneServerMenu) {
+        this.sceneServerMenu = sceneServerMenu;
     }
 }
